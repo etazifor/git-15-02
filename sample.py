c30c9dfbc0bd08d7b251c19dd65a5ab8d941933d
etazifor <devclass22@gmail.com>
2022-03-23 03:13:58 +0000

    changes made

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 #this is a dummy line
 
-print (" happy learning ")
+print (" happy learning'new concepts added' ")
 print (" this is an extra line ")
 print ("new line added")
 print ("fourth line")
